3b820d75b852dfe451c738964c1d870ebd7cf61f
Dinesh <dhineshok@gmail.com>
2023-03-14 15:31:54 +0530

    Add files via upload

---
+++ Mathematics/Count_Digits.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+/*Lets see an algorithm which can find the number of Digits of the number we give
+here we are having a function called "digits" which returns the number of digits*/
+int digits(int a)
+    {
+        int res=0;
+        while(a>0){
+            a=a/10;
+            res++;
+        }
+        return res;
+    }
+int main()
+{
+    
+    
+    int num;
+    cin >> num ;
+    int numdigit = digits(num);
+    cout << numdigit;
+    
+    return 0;
+}
