905d241cca14d6327db521d2077e6db5418e270f
Dinesh <dhineshok@gmail.com>
2023-03-27 15:57:43 +0530

    Add files via upload

--- Mathematics/Trailing-Zeros-in-Factorial.cpp
+++ Mathematics/Trailing-Zeros-in-Factorial.cpp
@@ -2,14 +2,9 @@
 using namespace std;
 /*Given an integer n, write a function that returns count of trailing zeroes in n!. */
 int trailingZerosInFactorial(int a){
-    int fact=1,b,zeros=0;
-    for(int i=1; i<=a; i++){
-        fact = fact*i;
-    }
-    int res =0;
-    while(fact%10==0){
-        res=res+1;
-        fact=fact/10;
+    int res = 0;
+    for(int i=5; i<=a; i=i*5){
+        res=res+a/5;
     }
     return res;
 }
