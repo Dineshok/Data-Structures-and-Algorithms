254acc9ec7a44274f96b16aa51b3fafc408d5e27
Dinesh <dhineshok@gmail.com>
2023-03-28 16:07:59 +0530

    Update GCD-of-two-Numbers.cpp

--- Mathematics/GCD-of-two-Numbers.cpp
+++ Mathematics/GCD-of-two-Numbers.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
-int gcdOfTwoNumbers(int n,int m){
+//This is Simple function to find GCD
+/*int gcdOfTwoNumbers(int n,int m){
     int res=min(n,m);
     while(res>0){
         if(n%res==0 && m%res==0){
@@ -9,6 +10,25 @@ int gcdOfTwoNumbers(int n,int m){
         res--;
     }
     return res;
+}*/
+//This can be done by - EUCLIDEAN Algorithm
+/*int gcdOfTwoNumbers(int a, int b){
+    while(a!=b){
+        if(a>b){
+            a=a-b;
+        }
+        else{
+            b=b-a;
+        }
+    }
+    return a;
+}*/
+//Simplified EUCLIDEAN Algorithm
+int gcdOfTwoNumbers(int a, int b){
+    if(b==0){
+        return a;
+    }
+    return gcdOfTwoNumbers(b,a%b);
 }
 int main(){
     int a,b;
